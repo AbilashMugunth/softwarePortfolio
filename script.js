54a6256f160cd336c34980b1b089641911eba1c1
Abilash <abimugunthan2000@gmail.com>
2022-12-20 21:30:47 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const menuBars = document.getElementById("menu-bars");
+const overlay = document.getElementById("overlay");
+const nav1 = document.getElementById("nav-1");
+const nav2 = document.getElementById("nav-2");
+const nav3 = document.getElementById("nav-3");
+const nav4 = document.getElementById("nav-4");
+const navItems = [nav1, nav2, nav3, nav4];
+
+// Control Navigation Animation
+function navAnimation(direction1, direction2) {
+  navItems.forEach((nav, i) => {
+    nav.classList.replace(
+      `slide-${direction1}-${i + 1}`,
+      `slide-${direction2}-${i + 1}`
+    );
+  });
+}
+
+function toggleNav() {
+  // Toggle: Menu Bars Open/Closed
+  menuBars.classList.toggle("change");
+  // Toggle: Menu Active
+  overlay.classList.toggle("overlay-active");
+  if (overlay.classList.contains("overlay-active")) {
+    // Animate In - Overlay
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+    // Animate In - Nav Items
+    navAnimation("out", "in");
+  } else {
+    // Animate Out - Overlay
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+    // Animate Out - Nav Items
+    navAnimation("in", "out");
+  }
+}
+
+// Event Listeners
+menuBars.addEventListener("click", toggleNav);
+navItems.forEach((nav) => {
+  nav.addEventListener("click", toggleNav);
+});
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: "vertical",
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
